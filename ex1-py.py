6923623c6575fbeae9d2379fe23a23ebf647ed02
helliio <liangzh@stud.ntnu.no>
2015-01-15 20:38:53 +0100

    Add implementation in python

---
+++ ex1-py.py
@@ -0,0 +1,23 @@
+from threading import Thread
+
+i = 0
+
+def someThreadFunction1():
+	global i
+	for x in range(1000000):
+		i=i+1
+
+def someThreadFunction2():
+	global i
+	for x in range(1000000):
+		i=i-1
+
+def main():
+	someThread1 = Thread(target = someThreadFunction1, args = (),)
+	someThread2 = Thread(target = someThreadFunction2, args = (),)
+	someThread1.start()
+	someThread2.start()
+	someThread1.join()
+	someThread2.join()
+	print(i)
+main()
