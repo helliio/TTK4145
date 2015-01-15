e5a84b20d226de0c37070f83ade8eb3ae4d39f39
Aleksander Wasaznik <aleksanw@pvv.ntnu.no>
2015-01-15 20:28:39 +0100

    EX1: Add implementation in go
    This code is supposed to demostrate race-conditions.

---
+++ excercise-1/ex1-go.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    . "fmt"
+	"runtime"
+	"time"
+)
+
+const iterations = 1000000
+
+var accumulator int
+
+func increment() {
+    for i := 0; i < iterations; i++ {
+        accumulator++
+    }
+}
+
+func decrement() {
+    for i := 0; i < iterations; i++ {
+        accumulator -= 1
+    }
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	go increment()
+	go decrement()
+
+	time.Sleep(100 * time.Millisecond)
+
+	Println(accumulator)
+}
