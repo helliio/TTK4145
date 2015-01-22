85b93fd4551e8aec8de473f77353557bf713faf9
helliio <liangzh@stud.ntnu.no>
2015-01-22 20:05:08 +0100

    Ex2 py: Add lock for threads

--- excercise-2/ex2-py.py
+++ excercise-2/ex2-py.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Lock
 
 i = 0
 iterations = 1000000
@@ -6,29 +6,55 @@ iterations = 1000000
 
 class Concurrent(Thread):
 	
-	def __init__(self, type, iterations):
+	"""
+	Thread doing the iterarions.
+	"""
+	
+	def __init__(self, type, iterations,lock):
+		"""
+		Constructor
+		"""
 		Thread.__init__(self)
 		self.type = type
+		self.lock = lock
 		self.iterations = iterations
 	
 	def increase(self):
+		"""
+		Increment global i
+		"""
 		global i
 		for x in range(self.iterations):
+			self.lock.acquire()
 			i = i + 1
+			self.lock.release()
 
 	def decrease(self):
+		"""
+		decrement global i
+		"""
 		global i
 		for x in range(self.iterations):
+			self.lock.acquire()
 			i = i - 1
+			self.lock.release()
+
 	
 	def run(self):
+		"""
+		Thread run function
+		"""
 		if self.type == "inc":
 			self.increase()
 		if self.type == "dec":
 			self.decrease()
 def main():
-	thread_inc = Concurrent("inc", iterations)
-	thread_dec = Concurrent("dec", iterations)
+	"""
+	Main function
+	"""
+	lock = Lock()
+	thread_inc = Concurrent("inc", iterations, lock)
+	thread_dec = Concurrent("dec", iterations, lock)
 	thread_inc.start()
 	thread_dec.start()
 	thread_inc.join()
