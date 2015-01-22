d300ea6c15b954195e5df8a35c04c7fa13b29328
helliio <liangzh@stud.ntnu.no>
2015-01-22 12:32:22 +0100

    EX2 py: Move increase, decrease to a class

--- excercise-2/ex2-py.py
+++ excercise-2/ex2-py.py
@@ -3,19 +3,32 @@ from threading import Thread
 i = 0
 iterations = 1000000
 
-def increase():
-	global i
-	for x in range(iterations):
-		i = i + 1
 
-def decrease():
-	global i
-	for x in range(iterations):
-		i = i - 1
+class Concurrent(Thread):
+	
+	def __init__(self, type, iterations):
+		Thread.__init__(self)
+		self.type = type
+		self.iterations = iterations
+	
+	def increase(self):
+		global i
+		for x in range(self.iterations):
+			i = i + 1
 
+	def decrease(self):
+		global i
+		for x in range(self.iterations):
+			i = i - 1
+	
+	def run(self):
+		if self.type == "inc":
+			self.increase()
+		if self.type == "dec":
+			self.decrease()
 def main():
-	thread_inc = Thread(target = increase, args = (),)
-	thread_dec = Thread(target = decrease, args = (),)
+	thread_inc = Concurrent("inc", iterations)
+	thread_dec = Concurrent("dec", iterations)
 	thread_inc.start()
 	thread_dec.start()
 	thread_inc.join()
