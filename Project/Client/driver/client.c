c39c371859b8a9e5991751536628f2170851e557
helliio <liang@dhcp-043137.wlan.ntnu.no>
2015-04-22 15:02:14 +0200

    more functions (WIP)

--- Project/Client/driver/client.c
+++ Project/Client/driver/client.c
@@ -2,45 +2,150 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int io_pannels[3][N_FLOORS];
+int commands[3][N_FLOORS];
 int move_dir = 0;
-int location = -1;
 
 void initialize_io_pannel(void){
     int i, j;
     for (i=0; i<3; i++){
         for (j=0; j<N_FLOORS; j++){
-            io_pannels[i][j]=0;
+            commands[i][j]=0;
         }
     }
 }
 
 void move_elevator(int dir){
-    
-}
-
-void open_and_close_door(void){
-    elev_set_door_open_lamp(1);
-    delay(15000);
-    elev_set_door_open_lamp(0);
-    return;
-}
-
-void stop_elev_at_next_floor(void){
     int i = elev_get_floor_sensor_signal();
     if (i != -1) {
-        elev_set_motor_direction(DIRN_STOP);
-        open_and_close_door();
-        turn_off_lights_at_floor(i);
+        if (dir > 0 && i < N_FLOORS-1) {
+            elev_set_motor_direction(DIRN_UP);
+            return;
+        }
+        if (dir == 0) {
+            elev_set_motor_direction(DIRN_STOP);
+            return;
+        }
+        if (dir < 0 && i > 0) {
+            elev_set_motor_direction(DIRN_DOWN);
+            return;
+        }
     }
+    if (i == -1) {
+        if (dir > 0) {
+            elev_set_motor_direction(DIRN_UP);
+            return;
+        }
+        if (dir == 0) {
+            elev_set_motor_direction(DIRN_STOP);
+            return;
+        }
+        if (dir < 0) {
+            elev_set_motor_direction(DIRN_DOWN);
+            return;
+        }
+    }
+}
+
+void execute_orders(void){
+    int i;
+    int commands = 0;
+    int loc = elev_get_floor_sensor_signal();
+    if (loc == -1) {
+        return;
+    }
+    for (i=0;i<N_FLOORS-1;i++) {
+        if (commands[0][i]==1 || commands[1][i]==1 || commands[2][i]==1) {
+            commands = 1;
+        }
+        if (move_dir == 0) {
+            if (commands[0][i]==1 || commands[1][i]==1 || commands[2][i]==1) {
+                if (loc > i) {
+                    move_dir = -1;
+                    move_elevator(-1);
+                    return;
+                }
+                if (loc < i) {
+                    move_dir = 1;
+                    move_elevator(1);
+                    return;
+                }
+                if (loc == i) {
+                    stop_and_open();
+                    return;
+                }
+            }
+        }
+        if (move_dir > 0) {
+            if (loc == N_FLOORS) {
+                stop_and_open();
+                move_dir = 0;
+                return;
+            }
+            if (commands[0][i]==1 && loc == i) {
+                stop_and_open();
+                move_elevator(1);
+                return;
+            }
+            if (commands[1][i]==1 && loc == i) {
+                stop_and_open();
+                move_elevator(1);
+                return;
+            }
+            
+        }
+        if (move_dir < 0) {
+            if (loc == 0) {
+                stop_and_open();
+                move_dir = 0; 
+            }
+            if (commands[0][i]==1 && loc == i) {
+                stop_and_open();
+                move_elevator(-1);
+                return;
+            }
+            if (commands[2][i]==1 && loc == i) {
+                stop_and_open();
+                move_elevator(-1);
+                return;
+            }
+        }
+    }
+    if (commands == 0) {
+        elev_set_motor_direction(DIRN_STOP);
+        move_dir = 0; 
+    }   
+}
+
+void stop_and_open(void){
+    int i = elev_get_floor_sensor_signal();
+    elev_set_motor_direction(DIRN_STOP);
+    turn_off_lights_at_floor(i);
+    elev_set_door_open_lamp(1);
+    delay(10000);
+    elev_set_door_open_lamp(0);
+    if (move_dir == 0) {
+        commands[0][i] = 0;
+        commands[1][i] = 0;
+        commands[2][i] = 0;
+    }
+    if (move_dir > 0) {
+        commands[0][i] = 0;
+        commands[1][i] = 0;
+    }
+    if (move_dir < 0) {
+        commands[0][i] = 0;
+        commands[2][i] = 0;
+    }
+
+    return;
 }
 
 void turn_off_lights_at_floor(int i){
     if (i >= 0 && i <= N_FLOORS-1) {
-        if(i < N_FLOORS-1){ 
+        if(i < N_FLOORS-1 && move_dir > 0){ 
             elev_set_button_lamp(BUTTON_CALL_UP, i, 0);
         }
-        if(i > 0){
+        if(i > 0 && move_dir < 0){
             elev_set_button_lamp(BUTTON_CALL_DOWN, i, 0);
         }
         elev_set_button_lamp(BUTTON_COMMAND, i, 0);
@@ -61,20 +166,20 @@ void listen_to_io_panels(void){
     for (i = 0; i < N_FLOORS; i++){
         if(elev_get_button_signal(BUTTON_COMMAND, i) == 1){
             elev_set_button_lamp(BUTTON_COMMAND, i, 1);
-            io_pannels[0][i]=1;
+            commands[0][i]=1;
             printf("Button COMMAND floor ",i+1," Pushed\n");
         }
         if(i < N_FLOORS-1){
             if(elev_get_button_signal(BUTTON_CALL_UP, i) == 1){
                 elev_set_button_lamp(BUTTON_CALL_UP, i, 1);
-                io_pannels[1][i]=1;
+                commands[1][i]=1;
                 printf("Button UP floor ",i+1," Pushed\n");
             }
         }
         if(i > 0){
             if(elev_get_button_signal(BUTTON_CALL_DOWN, i) == 1){
                 elev_set_button_lamp(BUTTON_CALL_DOWN, i, 1);
-                io_pannels[2][i]=1;
+                commands[2][i]=1;
                 printf("Button Down floor ",i+1," Pushed\n");
             }
         }
--- Project/Client/driver/client.h
+++ Project/Client/driver/client.h
@@ -19,15 +19,10 @@ Listen to the IO panel
 void listen_to_io_panels(void);
 
 /**
-Stop the elevator at the next floor it passes
-*/
-void stop_elev_at_next_floor(void);
-
-/**
-Open and close door
+Stop the motor and open-close the doors this also resets the command array
 */
 
-void open_and_close_door(void);
+void stop_and_open(void);
 
 /**
 Turn off light at designated floor
@@ -35,6 +30,11 @@ Turn off light at designated floor
 void turn_off_lights_at_floor (int i);
 
 /**
-Move the elevator and it will stop at any floor that has a stop flag
+Move the elevator and it will stop at any floor that has a stop flag -1 down 1 up 0 stop.
 */
 void move_elevator(int dir);
+
+/**
+Execute orders from the array
+*/
+void execute_orders(void);
